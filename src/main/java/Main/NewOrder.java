fd3147651b4fdf83bc06b6a50f536062d8ec1f66
GabrielMarconatto-terra <gabriel.marconatto_compasso@terrainvestimentos.com.br>
2023-12-07 13:53:30 -0300

    feat: criado produtor em java

---
+++ src/main/java/Main/NewOrder.java
@@ -0,0 +1,40 @@
+package Main;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.metrics.stats.Value;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrder
+{
+    public static void main(String[] args) throws ExecutionException, InterruptedException
+    {
+        var producer = new KafkaProducer<String, String>(properties());
+
+
+        var value = "123,234,345";
+        var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value);
+
+        producer.send(record, (data, ex) -> {
+            if(ex != null){
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println("sucesso " + data.topic() + ":::partition" + data.partition() +
+                    "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
+        }).get();
+    }
+
+    private static Properties properties(){
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
+}
